b8860d1af10351c9035b4f6054e10eb801d97d8b
Nataly Galushka <37546130+hellared@users.noreply.github.com>
2021-03-26 23:21:34 +0200

    Add tests on getPetByStatu

--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -1,10 +1,38 @@
 import got from "got";
 import { strict as assert } from "assert";
+import { URLSearchParams } from "url";
 
-describe("Pet", function() {
-    it("can be received by his id", async function() {
-        const response = await got("http://93.126.97.71:10080/api/pet/2")
+describe("Pet", function () {
+    it("can be received by his id", async function () {
+        const response = await got("http://93.126.97.71:10080/api/pet/2");
         const body = JSON.parse(response.body);
-        assert(body.id == 2, `Expected API return pet with id 2, but instead got with ${body.id}`)
+        assert(body.id == 2, `Expected API return pet with id 2, but instead got with ${body.id}`);
     })
+    it("can be received by status", async function () {
+        let response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
+            searchParams: { status: "available" }
+        });
+        let body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
+            searchParams: { status: "pending" }
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
+            searchParams: { status: "sold" }
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+
+        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
+            searchParams: new URLSearchParams( { status: ["sold", "available"] })
+        });
+        body = JSON.parse(response.body);
+        assert(body.length > 0);
+    });
+
+    it("can be received by tags", async function () { });
 });
