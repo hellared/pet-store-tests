41c3d9e01ca405626a9d403e0c672a2324072bee
Nataly Galushka <37546130+hellared@users.noreply.github.com>
2021-03-26 23:50:25 +0200

    Add api controller

--- tests/pet.test.ts
+++ tests/pet.test.ts
@@ -1,38 +1,36 @@
-import got from "got";
 import { strict as assert } from "assert";
-import { URLSearchParams } from "url";
+import { PetController } from "../api/controller/pet.controller";
+
+const pet = new PetController();
 
 describe("Pet", function () {
     it("can be received by his id", async function () {
-        const response = await got("http://93.126.97.71:10080/api/pet/2");
-        const body = JSON.parse(response.body);
+        const body = await pet.getById(2);
         assert(body.id == 2, `Expected API return pet with id 2, but instead got with ${body.id}`);
     })
     it("can be received by status", async function () {
-        let response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
-            searchParams: { status: "available" }
-        });
-        let body = JSON.parse(response.body);
+        let body = await pet.findByStatus("available");
         assert(body.length > 0);
 
-        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
-            searchParams: { status: "pending" }
-        });
-        body = JSON.parse(response.body);
+        body = await pet.findByStatus("pending");
         assert(body.length > 0);
 
-        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
-            searchParams: { status: "sold" }
-        });
-        body = JSON.parse(response.body);
+        body = await pet.findByStatus("sold");
         assert(body.length > 0);
 
-        response = await got("http://93.126.97.71:10080/api/pet/findByStatus", {
-            searchParams: new URLSearchParams( { status: ["sold", "available"] })
-        });
-        body = JSON.parse(response.body);
+        body = await pet.findByStatus(["pending", "available"]);
         assert(body.length > 0);
+        assert(body.some((pet: any) => pet.status == "pending"));
+        assert(body.some((pet: any) => pet.status == "available"));
+        assert(!body.some((pet: any) => pet.status == "sold"));
     });
-
-    it("can be received by tags", async function () { });
+    it("can be received by tags", async function () {
+        const body = await pet.findByTags("tag1");
+        assert(body.length > 0);
+        assert(body.every(
+            (pet: any) => pet.tags.some(
+                (tag: any) => tag.name == "tag1")
+        )
+        );
+    });
 });
